8ef891163ebdfd613191a86b68c744cc49afdb90
Hamada14 <madoo.ayman14@gmail.com>
2015-12-06 18:24:29 +0200

    none

--- Board.h
+++ Board.h
@@ -1,9 +1,9 @@
 #ifndef BOARD_H_INCLUDED
 #define BOARD_H_INCLUDED
 
-
+void printBoard(void);
+void resetBoard(void);
 
 #endif // BOARD_H_INCLUDED
 #include <stdio.h>
-void printBoard(void);
-void resetBoard(void);
+
---
+++ Game.c
@@ -0,0 +1,5 @@
+int getInput(void)
+{
+        printBoard();
+
+}
