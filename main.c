1d7ea991be55c60d4a8c40a96651c703b6db01e2
Hamada14 <madoo.ayman14@gmail.com>
2015-12-07 20:54:57 +0200

    Add new functions to Game source files

--- main.c
+++ main.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "Board.h"
-#include "Game.h"
-#include "Save.h"
+
 int main()
 {
-    resetBoard();
-    printBoard();
-    saveGame();
+    printInterface();
 
 }
