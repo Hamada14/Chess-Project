d2af56cb246c0b8e861e11f45a24bea6fd593678
Mohammed Abdelbari <m7md337@gmail.com>
2015-12-09 20:33:28 +0200

    add some movement checks

--- Movement.h
+++ Movement.h
@@ -1,6 +1,9 @@
 #ifndef MOVEMENT_H_INCLUDED
 #define MOVEMENT_H_INCLUDED
-
-
+bool isFirstMove(void);
+bool isNotOccupied(void);
+void applyMove(void);
+bool isValidEat(char piece);
+void movePiece(void);
 
 #endif // MOVEMENT_H_INCLUDED
